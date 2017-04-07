b667183d82faf9a294513c553fca6b1a33897fcf
sagar sharma <sagarsharma9595@gmail.com>
2017-04-07 19:02:07 +0530

    5 April Code5 April Code5 April Code5 April Code5 April Code

---
+++ Project-28March/src/all/JdbcClass.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package all;
+
+/**
+ *
+ * @author Sagar
+ */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Random;
+/**
+ *
+ * @author sagar
+ */
+public class JdbcClass {
+    /*
+     * database name, username and password for a database
+     */
+    private static final String dbName = "log";
+    private static final String username = "root";
+    private static final String password = "sagar123";
+  
+    
+    /*
+     * JDBC Driver details
+    */
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    
+    Connection conn = null;
+    Statement stmt = null;
+    
+    
+    void checkExistence(){
+        int flag = 0;
+        
+        
+         try{
+             
+            Class.forName("com.mysql.jdbc.Driver");
+            conn=DriverManager.getConnection(DB_URL, username, password);
+            ResultSet rs = conn.getMetaData().getCatalogs();
+           
+            while(rs.next()){
+                String cat = rs.getString(1);
+                if(dbName.equals(cat)){
+                    System.out.println(cat);
+                    flag = 1;
+                    conn.setCatalog(dbName);  
+                    break;
+                }
+            }
+            
+            if(flag == 0){
+                createDatabase();
+            }
+            
+            
+        
+         }
+         
+         catch(ClassNotFoundException | SQLException e){
+            //System.out.println("Problem");
+            e.printStackTrace();
+        }
+    }
+    
+    String randomName(){
+        Random r = new Random();
+        String id;
+        
+        while(true){
+            int num = r.nextInt(9000000) + 1000000;
+            id =  Integer.toString(num);
+            try{
+                stmt = conn.createStatement();
+                String str = "select * from info where folderID = '" + id + "'";
+                 ResultSet rs = stmt.executeQuery(str);
+                    if(rs.next() == false){
+                            return id;
+                    }
+                 
+            } catch (SQLException ex) {
+            Logger.getLogger(JdbcClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+    }
+    
+    void createDatabase(){
+        try{
+        Statement stmta = conn.createStatement();
+        stmta.executeUpdate("create database log");
+        conn.setCatalog(dbName);  
+        stmta = conn.createStatement();
+        String s = " create table info " +
+                " ( folderID Integer not null, " +
+                " name varchar (255) not null, " + 
+                " password varchar(255) not null, " +
+                " algorithm varchar (255) not null, " +
+                " transformation varchar (255) not null, " +
+                 " primary key (name, password), unique key(folderID) )";
+        stmta.executeUpdate(s);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+  
+    ResultSet findID(String user, String pass){  // use rs.getString(1); to take string, use rs.next() to check if String exists
+        try{
+        stmt = conn.createStatement();
+        String str = "select folderID, algorithm, transformation from info where password = '" + pass
+                + "' and name = '" + user  + "'";
+        ResultSet rs = stmt.executeQuery(str);
+        if(rs.next() == false){
+            return null;
+        }
+        else 
+            return rs;
+         
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    void saveID(String name, String pass, String algo, String trans, String fold){
+        try{
+           stmt = conn.createStatement();
+           String s = "insert into info (name, password, algorithm, transformation, folderID ) "
+                   + " values ( '" + name + "' , '" + pass +  "' , '"  + algo + "', '"
+                   + trans + "' , '" + fold + "' )";
+           stmt.executeUpdate(s);
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+    }
+    void closeConnection(){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(JdbcClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    void removeEntry(String id){
+        
+           try{
+           stmt = conn.createStatement();
+           String s = "delete from  info where folderID = '" + id  + " '";
+           stmt.executeUpdate(s);
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }      
+    }
+    
+   /* public static void main(String[] args){
+        JdbcClass h = new JdbcClass();
+        h.checkExistence();
+        h.saveID("sagar", "sharma", "Blowfish", "Blowfish", "2234567");
+   //     ResultSet s =h.findID("sagar", "sharma");
+     /*   try {
+           System.out.println(s.getString(1) + " " + s.getString(2)  + " "  + s.getString(3));
+        } catch (SQLException ex) {
+            Logger.getLogger(JdbcClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        h.closeConnection();
+    }*/
+}
---
+++ Project-28March/src/all/UI.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package all;
+
+/**
+ *
+ * @author Sagar
+ */
+
+import static all.ProjPID.Integration;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class UI {
+
+    public static void DirectoryMove(String location, String destination){
+        File src = new File(location);
+        File dest = new File(destination);
+        
+        try{
+            FileUtils.moveDirectoryToDirectory(src, dest, true);
+        } catch (IOException ex) {
+            Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    private static void showMenu1(){
+        System.out.println("1: New User");
+        System.out.println("2: Existing User");
+         System.out.println("3: To Exit");
+    }
+    
+    private static void showMenu2(){
+        System.out.println("Enter your details");
+    }
+    
+    public static void main(String[] args) throws SQLException{
+    
+        JdbcClass  db = new JdbcClass();  // Database Creation
+        db.checkExistence();
+           
+        OUTER:
+        while (true) {
+            showMenu1();
+            Scanner sc = new Scanner(System.in);
+            int choice;
+            ResultSet rs;
+            String algo, trans, user, pass, id, path;
+            choice = sc.nextInt();
+            switch (choice) {
+          
+                case 1:
+              
+                  System.out.println("Enter the username and password");
+                  sc.nextLine();
+                  user = sc.nextLine();
+                  pass = sc.nextLine();
+                  rs = db.findID(user, pass);
+                  if(rs != null){
+                      System.out.println("This user already exists, plz enter different username");
+                      break;
+                  }
+                  id = db.randomName();
+                  System.out.println("Enter the algorithm and transformation to be used");
+                  algo = sc.nextLine();
+                  trans = sc.nextLine();
+                  System.out.println("Enter the location of folder");
+                  path = sc.nextLine();
+                  File fc = new File(path);
+                  if(!fc.exists()){
+                      System.out.println("Plz enter correct path");
+                      break;
+                  }
+                  DirectoryMove(path, "D:\\DSS\\decryption\\" + id);
+                  db.saveID(user, pass, algo, trans, id);
+                  Zip z = new Zip(id);
+                  Encryption e = new Encryption(algo, trans, "Maryasda" , id);
+            {
+                try {
+                    z.zipFolder();
+                     e.encrypt();
+                
+                } catch (Exception ex) {
+                    Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+                   fc = new File("D:\\DSS\\decryption\\" + id + ".zipe");
+                  fc.delete();
+                  ProjPID p = new ProjPID();
+                  p.deleteDirectory(new File("D:\\DSS\\decryption\\" + id));
+                  break;
+                case 2:
+                    showMenu2();
+                   
+                    System.out.println("Enter username");
+                    sc.nextLine();
+                    user = sc.nextLine();
+                    System.out.println("Username is  " + user);
+                    System.out.println("Enter password");
+                    pass = sc.nextLine();
+                    System.out.println("Password is    " + pass);
+                    rs = db.findID(user, pass);
+                    // db.checkId();
+                    if(rs == null){
+                        System.out.println("Not a valid user, plz register first");
+                        continue;
+                    }      
+                    id = rs.getString(1);
+                    algo = rs.getString(2);
+                    trans = rs.getString(3);
+                    File f = new File("D:\\DSS\\encryption\\" + id + ".zip");
+                    System.out.println("Location is " + "D:\\DSS\\encryption\\" + id + ".zip");
+                    if(!f.exists()){
+                        db.removeEntry(id);
+                        System.out.println("Removed entry from db2");
+                        continue;
+                    }
+            {
+                try {
+                    Integration(id, algo, trans);
+                } catch (IOException | InterruptedException ex) {
+                    Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+                    
+                    
+                    
+                    break;
+                case 3:
+                    System.out.println("GoodBye!!");
+                    break OUTER;
+                default:
+                    System.out.println("Please enter correct choice");
+                    break;
+            }
+        }
+        
+        db.closeConnection();
+    }
+
+}
